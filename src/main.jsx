7890f7fad702b72b017be4705f4dfe5ed73500e8
sakina <sakinakalwar@gmail.com>
2026-06-13 19:13:26 +0500

    l

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <BrowserRouter> */}
+
     <App />
-    {/* </BrowserRouter> */}
+    
     
   </StrictMode>,
 )
